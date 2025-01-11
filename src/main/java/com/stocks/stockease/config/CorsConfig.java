98c3d2dc4886f110250baded44d4fffb7913d511
Keglev <ckbuzin1@gmail.com>
2025-01-11 14:16:51 +0100

    Updates: 1) Created a class named CorsConfig for handle the CORS implementation

---
+++ src/main/java/com/stocks/stockease/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.stocks.stockease.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:5173") // Frontend origin
+                .allowedMethods("*") // Allow all HTTP methods (GET, POST, etc.)
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (e.g., cookies)
+    }
+}
